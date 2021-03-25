f7bb277c44fc2442d9936abbd89eded90e4c0d55
John Schottler <jcschottler1991@gmail.com>
2021-03-24 18:36:08 -0600

    eslint issue reproduced

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  globals: {
+    window: true,
+  },
+  plugins: ['svelte3'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  settings: {
+    'svelte3/ignore-warnings': warning => {
+      if (warning.code === 'a11y-label-has-associated-control') return true // we have many labels without `for` attr
+      if (warning.code === 'no-unused-vars' && /'\$[^']+' is assigned a value but never used./.test(warning.message)) return true
+      return false
+    },
+  },
+  rules: {
+    // rules for both client and cypress
+    'no-undef': 'error',
+    'no-unreachable': 'error',
+    'no-unused-vars': 'error',
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'no-extra-semi': 'warn',
+    'no-useless-escape': 'warn',
+    'semi-spacing': 'warn',
+    'prefer-const': 'warn',
+    eqeqeq: 'off',
+    'no-case-declarations': 'off',
+    'no-extra-parens': 'off', // would be nice to turn this on and also make svelte files not put parens around lambda assignments like: () => (loading = true)
+    'no-irregular-whitespace': 'off',
+    'no-prototype-builtins': 'off',
+    semi: 'off',
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin')
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+	entry: {
+		'build/bundle': ['./src/main.js']
+	},
+	resolve: {
+		alias: {
+			svelte: path.dirname(require.resolve('svelte/package.json'))
+		},
+		extensions: ['.mjs', '.js', '.svelte'],
+		mainFields: ['svelte', 'browser', 'module', 'main']
+	},
+	output: {
+		path: path.join(__dirname, '/public'),
+		filename: '[name].js',
+		chunkFilename: '[name].[id].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						compilerOptions: {
+							dev: !prod
+						},
+						emitCss: prod,
+						hotReload: !prod
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				]
+			},
+			{
+				// required to prevent errors from Svelte on Webpack 5+
+				test: /node_modules\/svelte\/.*\.mjs$/,
+				resolve: {
+					fullySpecified: false
+				}
+			}
+		]
+	},
+	mode,
+	plugins: [
+		new ESLintPlugin({
+			failOnError: false,
+			extensions: ['svelte', 'js'],
+			// use cache/cacheStrategy in latest eslint when "error '$somestore' is assigned a value but never used  no-unused-vars" can either be ignored or is fixed in eslint-plugin-svelte3 or eslint-webpack-plugin
+			// cache: true,
+			// cacheStrategy: 'content',
+			fix: true // will change source files for lint that has potential fixes
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		})
+	],
+	devtool: prod ? false : 'source-map',
+	devServer: {
+		hot: true
+	}
+};
